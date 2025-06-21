8c852b7e774860f0fc5d64e7c2c198ec6c5028ce
142011551+briansantoss@users.noreply.github.com <142011551+briansantoss@users.noreply.github.com>
2025-06-20 21:06:11 -0300

    feat: base64 url functions returns 'bytes' object

--- base64url.py
+++ base64url.py
@@ -1,7 +1,7 @@
 BASE64URL_TABLE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_'
 GROUPS_LEN_BITS = 6
 
-def base64url_encode(input_bytes: bytes) -> str:
+def base64url_encode(input_bytes: bytes) -> bytes:
     # Convert input bytes to a single binary string
     input_bits = ''.join(f'{byte:08b}' for byte in input_bytes)
 
@@ -9,11 +9,11 @@ def base64url_encode(input_bytes: bytes) -> str:
     groups = [input_bits[i: i + GROUPS_LEN_BITS].ljust(GROUPS_LEN_BITS, '0') for i in
               range(0, len(input_bits), GROUPS_LEN_BITS)]
     # Map each 6-bit group to the corresponding Base64URL character
-    return ''.join(BASE64URL_TABLE[int(group, 2)] for group in groups)
+    return''.join(BASE64URL_TABLE[int(group, 2)] for group in groups).encode('ascii')
 
 
-def base64url_decode(encoded: str) -> bytes:
-    input_bits = ''.join(f'{BASE64URL_TABLE.index(char):0{GROUPS_LEN_BITS}b}' for char in encoded)
+def base64url_decode(encoded: bytes) -> bytes:
+    input_bits = ''.join(f'{BASE64URL_TABLE.index(chr(byte)):0{GROUPS_LEN_BITS}b}' for byte in encoded)
 
     # Group bits in 8-bits (bytes) blocks excluding incomplete ones
     blocks = [input_bits[i: i + 8] for i in range(0, len(input_bits) // 8 * 8, 8)]
